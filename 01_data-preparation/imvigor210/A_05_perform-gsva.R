747bdbd97d9e5d8133cbb2e9baf1af208b471bcd
KaiAragaki <adamaragaki@gmail.com>
2020-12-16 20:13:32 -0500

    Add check for genes, pois --> gaus

--- 01_data-preparation/imvigor210/A_05_perform-gsva.R
+++ 01_data-preparation/imvigor210/A_05_perform-gsva.R
@@ -14,14 +14,24 @@ library(GSVA)
 # Load Dataframes ---------------------------------------------------------
 
 imvigor <- read_rds("./data/imvigor210/norm_dds.Rds")
+
 signatures <- read_rds("./data/imvigor210/sig-with-null.Rds")
+
+
+# Ensure Genes are in Dataset ----------------------------------------
+
+genes <- unlist(signatures) %>% 
+        as_tibble()
+
+genes[which(!(genes$value %in% rownames(imvigor))),]
 
 
 # Run GSVA ----------------------------------------------------------------
 
-per_tumor_signature <- gsva(assay(imvigor, 2), signatures, mx.diff = T, kcdf = "Poisson")
+per_tumor_signature <- gsva(assay(imvigor, 2), signatures, mx.diff = T, kcdf = "Gaussian")
 
 
 # Export GSVA -------------------------------------------------------------
 
 write_rds(per_tumor_signature, "./data/imvigor210/gsva-scores.Rds")
+
